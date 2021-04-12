6a569cbeee656d2ab36fe64b5f27a71dd741d367
Nathaniel Filardo <nfilardo@microsoft.com>
2021-04-12 12:44:08 +0100

    CHERI AAL capptr_dewild

--- src/aal/aal_cheri.h
+++ src/aal/aal_cheri.h
@@ -67,5 +67,27 @@ namespace snmalloc
       return CapPtr<T, BOut>(static_cast<T*>(__builtin_cheri_address_set(
         a.unsafe_capptr, reinterpret_cast<ptraddr_t>(r.unsafe_capptr))));
     }
+
+    /**
+     * On CHERI, we want to ensure that the tag is still set on anything given
+     * to us.  Since we extract addresses (e.g., for amplification) we should
+     * also check that the cap is in bounds; however, it's simpler to test that
+     * the base is 0.  (That does not obviate the need for checking that we are
+     * at the start of an object, since the client may have set bounds to a
+     * subset of an allocation.)
+     */
+    template<typename T>
+    static SNMALLOC_FAST_PATH CapPtr<T, CBAllocE>
+    capptr_dewild(CapPtr<T, CBAllocEW> p) noexcept
+    {
+      void* vp = p.unsafe_capptr;
+
+#ifdef CHECK_CLIENT
+      if ((!cheri_tag_get(vp)) || (cheri_offset_get(vp) != 0))
+        return nullptr;
+#endif
+
+      return CapPtr<void, CBAllocE>(vp);
+    }
   };
 } // namespace snmalloc
