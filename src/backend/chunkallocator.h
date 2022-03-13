1a7bacd90d179976573e40f5092637147bd1bdcb
Nathaniel Wesley Filardo <nfilardo@microsoft.com>
2022-03-13 15:43:55 +0000

    Move MetaEntry and MetaCommon to backend
    These are almost entirely backend concerns, so move their definitions over
    there.  Use C++ friend classes to ensure that MetaCommon structures are opaque
    to frontend code (at least, at compile time, and neglecting the rest of C++).
    (These structures contain high-authority pointers and so should be as closely
    guarded as we can make them.)
    
    The bits that leak out are
    
    - the encoding of RemoteAllocator* and sizeclass_t into the uinptr_t within a
      MetaEntry.  This, however, is almost entirely a frontend concern, so detach
      the method definitions from the class and leave those in mem/metaslab.h for
      the moment.
    
    - the size of metadata structures pointed to by the MetaEntry meta field.
      Rather than use sizeof(Metaslab) (and assert that sizeof(ChunkRecord) is
      smaller), instead, define PAGEMAP_METADATA_STRUCT_SIZE once and assert that
      all records fit.  Additionally, add an assertion that Metaslab is exactly this
      size, not for semantic reasons, but because we expect it to be true.
    
    The bits that leak in are
    
    - the need to zero memory corresponding to a chunk.  Rather than having an
      escape hatch that reveals the MetaCommon.chunk, move the zeroing call into a
      small wrapper method within the MetaCommon class itself.
    
    - the need to get the address of a chunk.  We want to assert that we've got the
      right chunk on occasion (well, at least once so far) and so add a class method
      to expose the address_t view of the chunk pointer without exposing the pointer
      itself.

--- src/backend/chunkallocator.h
+++ src/backend/chunkallocator.h
@@ -7,6 +7,7 @@
  */
 
 #include "../backend/backend_concept.h"
+#include "../backend/metatypes.h"
 #include "../ds/mpmcstack.h"
 #include "../ds/spmcstack.h"
 #include "../mem/metaslab.h"
@@ -32,18 +33,15 @@ namespace snmalloc
   static_assert(std::is_standard_layout_v<ChunkRecord>);
   static_assert(
     offsetof(ChunkRecord, meta_common) == 0,
-    "ChunkRecord and Metaslab must share a common prefix");
+    "ChunkRecord* must be pointer convertible to MetaCommon*");
+  static_assert(
+    sizeof(ChunkRecord) <= PAGEMAP_METADATA_STRUCT_SIZE,
+    "Chunkrecord must fit in the pagemap metadata record");
 
   /**
    * How many slab sizes that can be provided.
    */
   constexpr size_t NUM_SLAB_SIZES = Pal::address_bits - MIN_CHUNK_BITS;
-
-  /**
-   * Used to ensure the per slab meta data is large enough for both use cases.
-   */
-  static_assert(
-    sizeof(Metaslab) >= sizeof(ChunkRecord), "We conflate these two types.");
 
   /**
    * Number of free stacks per chunk size that each allocator will use.
@@ -256,7 +254,7 @@ namespace snmalloc
 #endif
 
       state.add_peak_memory_usage(slab_size);
-      state.add_peak_memory_usage(sizeof(Metaslab));
+      state.add_peak_memory_usage(PAGEMAP_METADATA_STRUCT_SIZE);
       // TODO handle bounded versus lazy pagemaps in stats
       state.add_peak_memory_usage(
         (slab_size / MIN_CHUNK_SIZE) * sizeof(MetaEntry));
